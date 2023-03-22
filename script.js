cff507e09af8d8fa65dbd23b7da083229b730909
BuildTools <adamlux5@gmail.com>
2023-03-22 21:49:37 +0100

    changed generation of website

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// container for cards
-const wrapper = document.querySelector(".wrapper")
-
 //fetch data from JSON
 fetch('html.json')
   .then(response => {
@@ -8,48 +5,110 @@ fetch('html.json')
       return response.json()
     }
   })
-  .then(data => {
-    // execute printing function
-    printCards(data)
+  .then(cards => {
+    // execute rendering function
+    renderPage(cards.cardList)
   })
   .catch(err => {
     console.log(err.message)
   })
 
-// printing function
-const printCards = data => {
-  data.cardList.map((card, index) => {
-    // set the template
-    const cardItem = document.createElement("div")
-    cardItem.classList.add("card")
-    cardItem.setAttribute('key', index)
-    cardItem.innerHTML += ` 
-                  <div class="card-title">
-                  </div>
-                 `
-    // fill the template with data
-    cardItem.querySelector(".card-title").innerText = card.name
-    card.code.map((snippet, index) => {
-      const visual = document.createElement('div')
-      visual.classList.add('card-visual')
-      visual.setAttribute('key', index)
-      visual.innerHTML = snippet
-      const code = document.createElement('div')
-      code.classList.add('card-code')
-      code.setAttribute('key', index)
-      code.innerText = snippet
-      cardItem.appendChild(visual)
-      cardItem.appendChild(code)
-    })
-    // append the card to the wrapper
-    wrapper.appendChild(cardItem)
+// rendering function
+const renderPage = (cards) => {
+  const main = document.querySelector('main')
+  main.appendChild(createCopiedLabel())
+  main.appendChild(createWrapper(cards))
+  setEventListeners()
+}
+
+// create copied label
+const createCopiedLabel = () => {
+  const copiedLabel = document.createElement('div')
+  copiedLabel.classList.add('copied')
+  copiedLabel.innerText = 'Skopiowano!'
+
+  return copiedLabel
+} 
+
+// create wrapper for cards
+const createWrapper = (cards) => {
+  const wrapper = document.createElement('div')
+  wrapper.classList.add('wrapper')
+
+  cards.forEach(card => {
+    wrapper.appendChild(createCardElement(card))
+  });
+
+  return wrapper
+}
+
+// create and append cards to wrapper
+const createCardElement = (card) => {
+  const cardElement = document.createElement('div')
+  cardElement.classList.add('card')
+
+  cardElement.appendChild(createCardTitleElement(card))
+
+  card.code.forEach(code => {
+    cardElement.appendChild(createCardVisualElement(code))
+    cardElement.appendChild(createCardCodeElement(code))
+  })
+
+  return cardElement
+}
+
+// create and append title to card
+const createCardTitleElement = card => {
+  const titleElement = document.createElement('div')
+  titleElement.classList.add('card-title')
+
+  titleElement.innerText = card.name
+
+  return titleElement
+}
+
+// create and append visual to card
+const createCardVisualElement = code => {
+  const visualElement = document.createElement('div')
+  visualElement.classList.add('card-visual')
+
+  visualElement.innerHTML = code
+
+  return visualElement
+}
+
+// create and append code to card
+const createCardCodeElement = code => {
+  const codeElement = document.createElement('div')
+  codeElement.classList.add('card-code')
+
+  codeElement.innerText = code
+
+  return codeElement
+}
+
+// setting event listener for different actions
+const setEventListeners = () => {
+  const wrapper = document.querySelector('.wrapper')
+
+  wrapper.addEventListener('click', (e) => {
+    // execute copy to clipboard function
+    if (e.target.classList.contains('card-code')) {
+      copy(e)
+    }
+
+    // toggle accordions
+    if (e.target.classList.contains('card-title')) {
+      e.target.classList.toggle('closed')
+      e.target.parentElement.classList.toggle('closed')
+    }
   })
 }
 
-// copy to clipboard
-copiedLabel = document.querySelector('.copied')
 
+// copy to clipboard function
 const copy = (e) => {
+  const copiedLabel = document.querySelector('.copied')
   const toCopy = document.createElement('textArea')
   toCopy.value = e.target.innerText
   document.body.appendChild(toCopy)
@@ -61,19 +120,3 @@ const copy = (e) => {
     copiedLabel.classList.remove('slide')
   }, 1500)
 }
-
-wrapper.addEventListener('click', e => {
-  if (e.target.classList.contains('card-code')) {
-    copy(e)
-  }
-})
-
-
-// accordion
-
-wrapper.addEventListener('click', (e) => {
-  if(e.target.classList.contains('card-title')) {
-    e.target.classList.toggle('closed')
-    e.target.parentElement.classList.toggle('closed')
-  }
-})
